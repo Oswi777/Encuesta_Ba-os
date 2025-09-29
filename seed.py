20bf2031fa0d92e5718930ac5d944ecbd23416fd
Oswi777 <contreras.o@uadec.edu.mx>
2025-09-29 09:14:08 -0600

    seed.py actualizado

--- seed.py
+++ seed.py
@@ -10,9 +10,24 @@ schema_path = "schema.sql"
 pathlib.Path(db_path).parent.mkdir(parents=True, exist_ok=True)
 
 banos = [
-  ("B-A1-H1","Baño Hombres Ala 1 - Piso 1","Ala 1","1","Hombres",1),
-  ("B-A1-M1","Baño Mujeres Ala 1 - Piso 1","Ala 1","1","Mujeres",1),
-  ("B-A2-H2","Baño Hombres Ala 2 - Piso 2","Ala 2","2","Hombres",1),
+    ("RH-PB-H", "Baños RH – Planta Baja Hombres", "RH", "PB", "Hombres", 1),
+    ("RH-PB-M", "Baños RH – Planta Baja Mujeres", "RH", "PB", "Mujeres", 1),
+    ("RH-PA-H", "Baños RH – Planta Alta Hombres", "RH", "PA", "Hombres", 1),
+    ("RH-PA-M", "Baños RH – Planta Alta Mujeres", "RH", "PA", "Mujeres", 1),
+
+    # Planta 1
+    ("P1-ADM-H", "Baños Planta 1 – Administrativos Hombres", "Planta 1", "1", "Hombres", 1),
+    ("P1-ADM-M", "Baños Planta 1 – Administrativos Mujeres", "Planta 1", "1", "Mujeres", 1),
+    ("P1-PROD-H", "Baños Planta 1 – Producción Hombres", "Planta 1", "1", "Hombres", 1),
+    ("P1-PROD-M", "Baños Planta 1 – Producción Mujeres", "Planta 1", "1", "Mujeres", 1),
+
+    # Planta 2
+    ("P2-H", "Baños Planta 2 Hombres", "Planta 2", "2", "Hombres", 1),
+    ("P2-M", "Baños Planta 2 Mujeres", "Planta 2", "2", "Mujeres", 1),
+
+    # Planta 3
+    ("P3-H", "Baños Planta 3 Hombres", "Planta 3", "3", "Hombres", 1),
+    ("P3-M", "Baños Planta 3 Mujeres", "Planta 3", "3", "Mujeres", 1),
 ]
 
 con = sqlite3.connect(db_path); cur = con.cursor()
